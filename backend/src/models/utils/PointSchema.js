08b0c76536804046f6a16c2316becdd8b16dd216
Carlos Gustavo <carlosgustavohc@gmail.com>
2020-10-12 17:09:50 -0300

    Add files via upload

---
+++ backend/src/models/utils/PointSchema.js
@@ -0,0 +1,17 @@
+//const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+mongoose.set("useCreateIndex", true);
+
+const PointSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    enum: ["Point"],
+    required: true,
+  },
+  coordinates: {
+    type: [Number],
+    required: true,
+  },
+});
+
+module.exports = PointSchema;
